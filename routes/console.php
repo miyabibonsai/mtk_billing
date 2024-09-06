63c30ed4a5c7e83af3aefb1013b82fff1484cf1a
Min Thike Kyaw <minthike.mtk.710@gmail.com>
2024-09-06 20:30:24 +0630

    datasim generate

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('generate-billing simcard')->everyFiveMinutes();
+Schedule::command('generate-billing datasim')->everyThreeMinutes();
