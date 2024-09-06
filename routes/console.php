5791137924c29cf964a4a294a16cfc4022610de5
Min Thike Kyaw <minthike.mtk.710@gmail.com>
2024-09-06 22:12:03 +0630

    cron job tab

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('generate-billing datasim')->everyThreeMinutes();
+Schedule::command('generate-billing simcard')->everyFiveMinutes();
